017b8503fa16888e29eb82e74d48956f499f8939
reishort <rshort@wesleyan.edu>
2022-04-27 23:11:34 -0400

    rename screens

--- components/addScreen.js
+++ components/addScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { styles } from '../styles'
 import {View, Image, Text} from 'react-native';
-export default function FinalScreen(props) {
+export default function addScreen(props) {
     return (
         <View style={styles.container}>
             //need to change this to reflect search results
