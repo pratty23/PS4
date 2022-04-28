480c69d411c55fb7241c24a413b747aae65804ca
pratty23 <98915134+pratty23@users.noreply.github.com>
2022-04-27 22:49:20 -0400

    update initial screen, ratings not printing

--- components/initialScreen.js
+++ components/initialScreen.js
@@ -1,31 +1,74 @@
 import React, { useEffect, useState } from "react";
-//import axios from "axios";
-import openImagePickerAsync from '../imagePicker';
-import {styles} from '../styles';
-import {Image, StyleSheet, Button, Text, Pressable, View, TouchableOpacity, FlatList} from "react-native";
+// import the components we want to use
+//import MusicRater from './components/MusicRater'
+//import InitialScreen from './components/InitialScreen'
+import {FlatList, Text, View } from "react-native";
 
 
-export default function InitialScreen(props) {
-  return(
-  <View style={styles.container}>
-  // need to edit this to add the search function and display rankings
-    <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />
-    <Text style={styles.instructions}>
-      Welcome To Simply Music!
-      We Will Get Ya What Ya Need!
-    </Text>
+export default function App() {
+  // Initially, set isLoading to true and set up the setLoading function for
+  // later changing the isLoading value.
+  const [isLoading, setLoading] = useState(true);
+  // Initially, set data to an empty array and set up the setData function for
+  // later changing the data value to the fetched data.
+  const [data, setData] = useState([]);
 
-    <Pressable
-      hitSlop={30}
-      onPress={ async () => {
-          let image = await openImagePickerAsync();
-          image ? props.setImage(image) : null
-          }
-      }
-      style={styles.button}>
-      <Text style={styles.buttonText}>Pick a photo</Text>
-    </Pressable>
-  </View>
-  )
+  // The useEffect hook is similar to the componentDidMount and
+  // componentDidUpdate in class components. For our anonymoust function, we will
+  // have one parameter, fetch(), and an empty function body.
+  // Note that items in a Django database can be retrieved that way as well.
+  // Try it out with Postman.
+  useEffect(() => {
+    // Pass the URL to the fetch API.
+    fetch("https://simplymusic.herokuapp.com/api/rating")
+      // Parse the response object and extract the json response that is obtained.
+      .then((response) => response.json())
+      // Set the empty data variable to the fetched data.
+      .then((json) => setData(json))
+      // Catch any errors thrown from the fetch call.
+      .catch((error) => console.error(error))
+      // While the data is loading, show the isLoading view below.
+      // Once setLoading() sets isLoading to false, show the view with the
+      // loaded data.
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    // Now the component parses the data and renders it using a FlatList component.
+    <View style={{ flex: 1, padding: 24 }}>
+      {/* As long as isLoading is true, show "Loading ..." */}
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        // Once it is false, show the fetched data.
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <Text style={{ fontSize: 18, color: "green", textAlign: "center" }}>
+          </Text>
+          <Text
+            style={{
+              fontSize: 14,
+              color: "green",
+              textAlign: "center",
+              paddingBottom: 10,
+            }}
+          >
+            Ratings
+          </Text>
+          <FlatList
+            data={data.rating}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>{item.id + " : " + item.rating + " : " + item.username}</Text>
+            )}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
-
